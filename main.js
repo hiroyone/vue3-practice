f0ad32c3919785107e36fe126909a3ec4efbffcf
hiroyone <hirokiyonetani@gmail.com>
2021-08-19 11:49:02 +0900

    L5: List Rendering & L6:Event Handling

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ const app = Vue.createApp({
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
       inventory: 100,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+      ],
+      cart: 0,
     };
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
