7e18de8d2034e56b6206b535b5f93efd8c1390c7
hiroyone <yonetani@team-lab.com>
2021-08-23 15:59:47 +0900

    L9: Components & Props

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
 const app = Vue.createApp({
   data: function () {
     return {
-      product: "Socks",
-      image: "./assets/images/socks_green.jpg",
-      inventory: 0,
-      inStock: false,
-      details: ["50% cotton", "30% wool", "20% polyester"],
-      variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
-      ],
       cart: 0,
+      premium: true,
     };
   },
-  methods: {
-    addToCart: function () {
-      this.cart += 1;
-    },
-    updateImage(variantImage) {
-      this.image = variantImage;
-    },
-  },
+  methods: {},
 });
